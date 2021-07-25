592c0c976a7cba9b3097963e0c9b369ad099e870
alexb997 <alex_alexandru_997@yahoo.ro>
2021-07-25 19:23:14 +0300

    Created add & delete http calls

--- src/main/java/com/example/rest/controller/TodosController.java
+++ src/main/java/com/example/rest/controller/TodosController.java
@@ -1,9 +1,6 @@
 package com.example.rest.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,19 +8,26 @@ import java.util.List;
 @RestController
 @RequestMapping("/todos")
 public class TodosController {
-    private List<String> todos=new ArrayList<>();
+    private List<String> todos = new ArrayList<>();
 
     @GetMapping("/todos")
     public List<String> getTodos() {
         return todos;
     }
 
+    @PostMapping("/add")
+    public void postTodos(@RequestParam String todo) {
+        todos.add(todo);
+    }
+
+    @DeleteMapping("delete/{index}")
+    public void deleteTodos(@PathVariable(value = "index") int index) {
+        todos.remove(index);
+    }
+
     @GetMapping("/todo/{index}")
-    public String getTodo(@PathVariable(value = "index") int index){
+    public String getTodo(@PathVariable(value = "index") int index) {
         return todos.get(index);
     }
-    @GetMapping("/getGreeting/{name}")
-    public String getGreeting(@PathVariable(value = "name") String name) {
-        return "Hello " + name;
-    }
-}// List at class level, add todo and a delete todo;
+}
+
